f862ef534c5f69e78bb71e7a4f63477f04a5ac37
XXXHHHDynasty <421735132@qq.com>
2021-12-16 14:44:05 -0500

    ✨ feat: new sign in component

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { history, request } from 'umi';
 import { Menu, Modal, Dropdown, Avatar, Button, Form, Input } from 'antd';
 import { DownOutlined, UserOutlined } from '@ant-design/icons';
@@ -6,6 +6,48 @@ import SearchForm from '@/components/RightContent/SearchForm';
 
 export default () => {
   const userName = localStorage.getItem('ams_uname');
+  const [data, setdata] = useState<any>();
+
+  const userLogin = () => {
+    if (userName) {
+      return [
+        <Button
+          type="link"
+          onClick={() => {
+            localStorage.clear();
+          }}
+        >
+          {'Sign out'}
+        </Button>,
+      ];
+    } else {
+      return [
+        <>
+          <Button
+            type="link"
+            onClick={() => {
+              history.push('/create');
+            }}
+          >
+            {'Register'}
+          </Button>
+          <Button
+            type="link"
+            onClick={() => {
+              history.push('/login');
+            }}
+          >
+            {'Sign in'}
+          </Button>
+        </>,
+      ];
+    }
+  };
+
+  useEffect(() => {
+    const data = userLogin();
+    setdata(data);
+  });
 
   return (
     <>
@@ -17,15 +59,7 @@ export default () => {
       <div style={{ color: 'white' }}>
         {userName ? `Welcome! ${userName}` : 'Anonymous User'}
       </div>
-      <Button
-        type="link"
-        onClick={() => {
-          localStorage.clear(),
-            userName ? history.push('/login') : history.push('/create');
-        }}
-      >
-        {userName ? 'Sign out' : 'Register'}
-      </Button>
+      {data}
     </>
   );
 };
